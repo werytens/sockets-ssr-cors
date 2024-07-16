1a7354d72a697899668dbcacc45aaa587a9cf77f
Maksim Lipatov <Maksim.Lipatov@nedra.digital>
2024-07-16 15:52:19 +0300

    feat: change sockets

--- websockets/backend/index.js
+++ websockets/backend/index.js
@@ -1,37 +1,54 @@
-const express = require('express');
-const port = 4000;
-const cors = require('cors');
-const { Server } = require('socket.io');
-const { createServer } = require('node:http');
+const express = require('express')
+const port = 4000
+const cors = require('cors')
+const { Server } = require('socket.io')
+const { createServer } = require('node:http')
 
-const app = express();
-const server = createServer(app);
+const app = express()
+const server = createServer(app)
 const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:3001',
-    methods: ["GET", "POST"]
-  }
-});
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+})
 
-app.use(cors({ origin: true }));
+app.use(cors({ origin: true }))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    res.send('Hello World!')
+})
 
+//* Минусы сокетов
+// Необходимо оптимизировать в больших приложениях
+// WebSockets не оптимизированы для потоковой передачи аудио-и видеоданных (но можно переделать в блоб)
+// Если сокетов несколько - работать с ними сложно
+// Использование вебсокетов требует от разработчиков более глубокого понимания 
+// асинхронного программирования и работы с сетевыми протоколами.
+// Вебсокеты открывают новые возможности для атак, таких как атаки типа DOS через открытые соединения.
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.emit('chat message', socket.client.id + ' connected')
+    console.log('a user connected')
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    io.emit(
+        'clientsData',
+        JSON.stringify(Object.keys(io.sockets.server.eio.clients))
+    )
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+    io.emit('chat message', socket.client.id + ' connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+        io.emit(
+            'clientsData',
+            JSON.stringify(Object.keys(io.sockets.server.eio.clients))
+        )
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
 
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
--- websockets/frontend/src/components/Console.js
+++ websockets/frontend/src/components/Console.js
@@ -1,14 +1,15 @@
-import { useState, useEffect } from 'react';
-import { io } from 'socket.io-client';
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
-const URL = 'http://localhost:4000';
+const URL = "http://localhost:4000";
 
 const socket = io(URL);
 
 export const Console = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [messages, setMessages] = useState([]);
-  const [messageText, setMessageText] = useState('');
+  const [messageText, setMessageText] = useState("");
+  const [allClients, setClietns] = useState([]);
 
   useEffect(() => {
     function onConnect() {
@@ -20,33 +21,44 @@ export const Console = () => {
     }
 
     function onMessage(message) {
-        setMessages((prevMessages) => [...prevMessages, message]);
+      setMessages((prevMessages) => [...prevMessages, message]);
     }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('chat message', onMessage);
+    function onClientsDataGet(message) {
+      setClietns(JSON.parse(message));
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("chat message", onMessage);
+    socket.on("clientsData", onClientsDataGet);
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('chat message', onMessage);
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("chat message", onMessage);
     };
   }, []);
 
   const handleSendMessage = (e) => {
     e.preventDefault();
 
-    if (messageText !== '') {
-        socket.emit('chat message', `${socket.id}: ` + messageText)
-        setMessageText('');
+    if (messageText !== "") {
+      socket.emit("chat message", `${socket.id}: ` + messageText);
+      setMessageText("");
     }
-  }
+  };
 
   return (
     <section className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-2 rounded bg-[grey] min-h-[600px] min-w-[800px]">
       <h1 className="bg-black p-2 mb-2">
-        CONSOLE [{isConnected ? <span className="text-[green]">Online</span> : <span className="text-[red]">OFFLINE</span>}]
+        CONSOLE [
+        {isConnected ? (
+          <span className="text-[green]">Online</span>
+        ) : (
+          <span className="text-[red]">OFFLINE</span>
+        )}
+        ]
       </h1>
       <div className="bg-black h-[100%] min-h-[600px] p-2">
         <h2 className="bg-[grey] p-1 rounded mb-2">Messages:</h2>
@@ -55,14 +67,31 @@ export const Console = () => {
             <div key={index}>{msg}</div>
           ))}
         </div>
-        <form className='flex'>
-            <input placeholder='type ur message' className='outline-none bg-[grey] p-1 rounded w-[100%] resize-none' value={messageText} onChange={(e) => {
-                setMessageText(e.target.value)
-            }} />
-            <button className='bg-[grey] p-1 rounded ml-2 outline-none' onClick={handleSendMessage}>
-                send
-            </button>
+        <form className="flex">
+          <input
+            placeholder="type ur message"
+            className="outline-none bg-[grey] p-1 rounded w-[100%] resize-none"
+            value={messageText}
+            onChange={(e) => {
+              setMessageText(e.target.value);
+            }}
+          />
+          <button
+            className="bg-[grey] p-1 rounded ml-2 outline-none"
+            onClick={handleSendMessage}
+          >
+            send
+          </button>
         </form>
+      </div>
+
+      <div className="absolute top top-[50%] left-[-20%] translate-x-[-50%]">
+        <p>{allClients.length} clients</p>
+        <ul>
+          {allClients.map((id) => (
+            <li key={id}>{id}</li>
+          ))}
+        </ul>
       </div>
     </section>
   );
